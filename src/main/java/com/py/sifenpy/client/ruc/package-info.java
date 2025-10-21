b0f18619851b674c6a000223f99fc41a0c822399
Ignacio Salcedo <salcedo.ig82@gmail.com>
2025-10-20 21:28:33 -0300

    Dejar de rastrear código de cliente SOAP generado

--- src/main/java/com/py/sifenpy/client/ruc/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://ekuatia.set.gov.py/sifen/xsd", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.py.sifenpy.client.ruc;
